451718724836860fc7e6f0d284d4edf663e7d850
Rasis Syauqi Buldan <rasisbuldan@gmail.com>
2023-04-28 23:51:14 +0700

    add request log and echo page url

--- backend/main/main.py
+++ backend/main/main.py
@@ -4,18 +4,23 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route("/check-fact", methods=['POST'])
 def ask():
     reqJson = request.get_json()
+    print(reqJson)
     print("Page url: " + reqJson['pageUrl'])
     print(reqJson['factList'])
     print(reqJson['factList'][0])
     sampleDict = {
         "success": "true",
-        "hotel": "trivago"
+        "hotel": "trivago",
+        "pageUrl": reqJson['pageUrl']
     }
     return jsonify(sampleDict)
 
+
 if __name__ == '__main__':
+    print("Running app on port 8080...")
     from waitress import serve
     serve(app, host="127.0.0.1", port=8080)
