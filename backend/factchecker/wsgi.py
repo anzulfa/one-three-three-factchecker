467df9444d6dbc97a7a903760d99a62ee32042d4
Rasis Syauqi Buldan <rasisbuldan@gmail.com>
2023-04-29 17:13:00 +0700

    setup uwsgi

--- backend/factchecker/wsgi.py
+++ backend/factchecker/wsgi.py
@@ -1,5 +1,6 @@
-from summarize import app
+from factchecker import app
 
 if __name__ == "__main__":
+    print("Running wsgi app on port 8080...")
     from waitress import serve
     serve(app, host="127.0.0.1", port=8080)
