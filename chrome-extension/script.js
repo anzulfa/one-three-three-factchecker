70a0af1d3b188dc2e6b36edaa4a9fc8c8b2130db
anzulfa <anzulfa39@gmail.com>
2023-04-30 20:54:43 +0700

    ui v1

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,7 +1,31 @@
-async function fetchData() {
-    let response=await fetch ("http://127.0.0.1:5000/getisfact/")
-        .then(response => response.json());
-    document.getElementById("result").innerHTML = response.message;
-    console.log(response.message)
+function myFunction(response) {
+    document.getElementById("loader-container").style.display = "none"
+    document.getElementById("result-container").style.display = "block"
+    document.getElementById("result-grade").innerHTML = response.grade;
+    console.log(response)
+    console.log(response.accuratePoints[0])
 }
+
+async function fetchData() {
+    // let response1=await fetch ("http://103.82.93.200:8081/check-fact", {
+    //     method: "POST",
+    //     headers: {
+    //       'Accept': '*/*',
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(
+    //         {
+    //             pageUrl: "https://edition.cnn.com/2021/06/09/tech/robot-zaps-weeds-spc-intl/index.html"
+    //         }
+    //     )
+    // })
+    //     .then(response1 => response1.json())
+    //     .then(response1Json => console.log(response1Json));
+
+    await fetch ("http://127.0.0.1:5000/getisfact/")
+        .then(response => response.json())
+        .then(responseJson => this.myFunction(responseJson))
+ 
+}
+
 fetchData();
